ee7b26650c06fd57ffb84b9bb2cc694d34b5dfda
Pavel Lavrinovich <1avrinovich.pav31@gmail.com>
2016-07-14 11:30:08 +0300

    Question_id validation. Dependent destroy. Index. Foreign key.

--- db/migrate/20160713185701_add_question_id_to_answers.rb
+++ db/migrate/20160713185701_add_question_id_to_answers.rb
@@ -1,5 +1,5 @@
 class AddQuestionIdToAnswers < ActiveRecord::Migration
   def change
-    add_belongs_to :answers, :question
+    add_belongs_to :answers, :question, index: true, foreign_key: true
   end
 end
