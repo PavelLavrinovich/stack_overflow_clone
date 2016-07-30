d587ce7da3f947dea07e0327641c87ef9b38e91d
Pavel Lavrinovich <1avrinovich.pav31@gmail.com>
2016-07-30 23:42:36 +0300

    Add files to the question.

--- spec/acceptance/add_files_for_question_spec.rb
+++ spec/acceptance/add_files_for_question_spec.rb
@@ -16,7 +16,7 @@ feature 'User can add files for his question', %q{
     fill_in 'Title', with: 'some_title'
     fill_in 'Body', with: 'some_body'
     attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
-    click_on 'Cofirm'
+    click_on 'Confirm'
 
     expect(page).to have_content 'spec_helper.rb'
   end
